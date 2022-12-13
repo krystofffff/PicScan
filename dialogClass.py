1386ea98111887f22b64d5689aa0314bb5323070
Krystof Krmaschek <krystof2508@seznam.cz>
2022-12-13 12:35:21 +0100

    dialog zoom

--- dialogClass.py
+++ dialogClass.py
@@ -1,8 +1,10 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout, QScrollArea, QGraphicsView, QGraphicsScene, \
+    QGraphicsPixmapItem
 
 import graphicOperations as Go
 import dataManager as Dm
+from editController import MainView
 
 
 class Dialog(QDialog):
@@ -10,15 +12,18 @@ class Dialog(QDialog):
         super().__init__()
         self.idx = idx
         self.setWindowTitle("image")
-        image = Go.get_qpixmap(Dm.get_cutouts()[self.idx].img)
-        downscaled_image = image.scaled(1000, 800, Qt.KeepAspectRatio)
 
-        label = QLabel()
-        label.setPixmap(downscaled_image)
-        label.setMinimumSize(label.sizeHint())
+        pic = QGraphicsPixmapItem()
+        pixmap = Go.get_qpixmap(Dm.get_cutouts()[self.idx].img)
+        pic.setPixmap(pixmap)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(label)
+        self.scene = QGraphicsScene()
+        self.scene.addItem(pic)
+        self.graphicsView = MainView(self.scene)
 
-        self.setLayout(vbox)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.graphicsView)
+        self.setLayout(self.layout)
+
         self.exec_()
+
